279996e6625d79726e5c0b4bb7c6a6922dfc8dcd
Catherina Dela Cruz <delacruzcatherinaheart54@gmail.com>
2025-04-27 22:57:16 +0800

    database

--- database.js
+++ database.js
@@ -1,11 +1,10 @@
-// Example using MySQL (replace with your chosen database driver)
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'your_user',
-    password: 'your_password',
-    database: 'your_database'
+    user: 'root', // As seen in your SELECT USER(); output
+    password: 'mysecretpassword', // Replace with the password you used to log in to MySQL
+    database: 'dental_app' // The database you just created
 });
 
 connection.connect((err) => {
@@ -16,7 +15,6 @@ connection.connect((err) => {
     console.log('Connected to the database!');
 });
 
-// Function to execute secure database queries using parameterized queries
 const query = (sql, values) => {
     return new Promise((resolve, reject) => {
         connection.query(sql, values, (error, results) => {
